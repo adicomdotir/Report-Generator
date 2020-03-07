96e1ed7d2b0e32fdf497c18f8dbfe3f524953d7b
adicomdotir <adicom.ir@gmail.com>
2020-03-07 11:50:12 +0330

    Fix header bugs

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
 	selector: 'app-header',
@@ -7,15 +6,10 @@ import { TranslateService } from '@ngx-translate/core';
 	styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-	y = 5;
 
-	constructor(private translate: TranslateService) { }
+	constructor() { }
 
 	ngOnInit() { }
 
 	init() { }
-
-	changeLanguage(event) {
-		this.translate.use(event.target.value);
-	}
 }
