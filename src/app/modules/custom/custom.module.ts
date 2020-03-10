2eb3cad42a5ea94807973e94d48bae3fb1123de9
adicomdotir <adicom.ir@gmail.com>
2020-03-10 11:29:32 +0330

    Remove file and fix bugs

--- src/app/modules/custom/custom.module.ts
+++ src/app/modules/custom/custom.module.ts
@@ -4,31 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { CustomRoutingModule } from './custom-routing.module';
 import { HomeComponent } from './home/home.component';
-import { ReversePipe } from 'src/app/shared/pipes/reverse.pipe';
-import { TrainingComponent } from './training/training.component';
-import { HealingComponent } from './healing/healing.component';
-import { StadiumComponent } from './stadium/stadium.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { CustomRatingComponent } from './custom-rating/custom-rating.component';
-import { PositionDetailComponent } from './position-detail/position-detail.component';
-import { PlayerBoxComponent } from './custom-rating/player-box/player-box.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 		FormsModule,
-		CustomRoutingModule,
-		TranslateModule
+		CustomRoutingModule
 	],
 	declarations: [
-		HomeComponent,
-		ReversePipe,
-		TrainingComponent,
-		HealingComponent,
-		StadiumComponent,
-		CustomRatingComponent,
-		PositionDetailComponent,
-		PlayerBoxComponent // TODO: move to shared module
+		HomeComponent
 	]
 })
 export class CustomModule { }
